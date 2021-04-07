fa523455b3f20e125d42b171320eec67b231c9e2
rdavilao <rdavila@espe.edu.ec>
2021-04-07 16:10:58 -0500

    Changed IP from DomainConstant

--- src/main/java/ec/edu/espe/corebancario/accounts/constants/DomainConstant.java
+++ src/main/java/ec/edu/espe/corebancario/accounts/constants/DomainConstant.java
@@ -2,6 +2,6 @@ package ec.edu.espe.corebancario.accounts.constants;
 
 public class DomainConstant {
 
-    public static final String DOMAINLOGIN = "http://54.82.169.215:8084/api/corebancario/seguridad/login";
-    public static final String DOMAINCLIENT = "http://54.82.169.215:8081/api/corebancario/client";
+    public static final String DOMAINLOGIN = "http://3.234.240.187:8084/api/corebancario/seguridad/login";
+    public static final String DOMAINCLIENT = "http://3.234.240.187:8081/api/corebancario/client";
 }
